7358ccaf028083378613db7acf13dec6d7d5cc7e
Your Name <your_email@example.com>
2026-06-10 00:31:28 +0530

    Removed djangoProject folder from github tracking

--- pro1/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request,'index.html')
-
-def shila(request):
-    return render (request,'cba.html')
